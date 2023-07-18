0d0de8d16fdd6b5b7af012e8bfe07db36b8f34bf
opoberezh <opoberezh.2015@gmail.com>
2023-07-18 14:57:34 +0200

    added_audio

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,20 +5,27 @@ const els = {
     btnStart: document.querySelector('button[data-start]'),
     btnStop: document.querySelector('button[data-stop]'),
     body: document.querySelector('body'),
+   audioPlayer: document.querySelector('#audio-player'),
 }
 
 
-els.btnStart.addEventListener('click', onBtnStartChangeColor);
-els.btnStop.addEventListener('click', onBtnStopChangeColor);
+
+els.audioPlayer.style.cssText = `opacity: 0; margin-top: 500px;`;
+
+els.audioPlayer.volume = 0.2;
+
+els.btnStart.addEventListener('click', onBtnStartChangeColor, false);
+els.btnStop.addEventListener('click', onBtnStopChangeColor, true);
 
 function onBtnStartChangeColor(){
-    els.btnStart.disabled = true;
-    els.btnStop.disabled = false;
+     els.btnStart.disabled = true;
+     els.btnStop.disabled = false;
    
     idInt = setInterval(() =>{
         els.body.style.backgroundColor = getRandomHexColor()
     }, CHANGE_COLOR_DELAY);
-   
+    
+    playAudio ();
 }
 
 
@@ -27,8 +34,17 @@ function onBtnStopChangeColor(){
     els.btnStop.disabled = true;
 
     clearInterval(idInt);
-   
+    pauseAudio ();
 }
+
+function playAudio (){
+    els.audioPlayer.play();
+}
+
+function pauseAudio (){
+    els.audioPlayer.pause();
+}
+
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
